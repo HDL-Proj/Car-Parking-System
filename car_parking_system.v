d09b59ca376f3a3db46c8f2d74d3a20b5ca499b9
ProgrammerJaneki <revillazjannelz@gmail.com>
2023-01-13 16:40:52 +0800

    car parking walkthrough

--- car_parking_system.v
+++ car_parking_system.v
@@ -9,7 +9,9 @@ module car_parking_system (
   output reg empty_signal
 );
 
+// the var count is used to monitor the no. of cars
 reg [3:0] count;
+
 
 initial begin
   count = 4'b0;
@@ -17,19 +19,22 @@ initial begin
   empty_signal = 1'b1;
 end
 
+
 always @(posedge clock or posedge reset) begin
+  // asynchronous value/function
+  // this block updates the value of count based on car_arrival or car_departure
   if (reset) begin
     count <= 4'b0;
   end else begin
     if (car_arrival) begin
       if (count == 4'b1111) begin
-        count <= count;
+        count <= count; 
       end else begin
         count <= count + 1;
       end
     end else if (car_departure) begin
       if (count == 4'b0000) begin
-        count <= count;
+        count <= count; 
       end else begin
         count <= count - 1;
       end
@@ -37,6 +42,7 @@ always @(posedge clock or posedge reset) begin
   end
 end
 
+// this block is executed every time an input changes
 always @(*) begin
   full_signal = (count == 4'b1111);
   empty_signal = (count == 4'b0000);
